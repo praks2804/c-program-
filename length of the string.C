7df6124f7a516c21f22401a8eb4dc7eda0aa6dba
Prakruthi B <125117101+praks2804@users.noreply.github.com>
2023-03-23 19:04:08 +0530

    Create length of the string.C

---
+++ length of the string.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+  char str[100];
+  int i;
+  printf("enter the string:");
+  scanf("%s",str);
+  printf("length of the string is %d",strlen (str));
+  getch();
+}
